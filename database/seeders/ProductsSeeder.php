cb10dd0d7c1d1d1efe768cbf0cc84e1d78911cae
Scott Hellings <scott.hellings@netmatters-scs.com>
2022-01-14 11:45:47 +0000

    products: model, migration, factory and seeeder

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ProductsSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(ProductsSeeder::class);
+       Product::factory(20)->create();
     }
 }
