82a72ea6a777f87c5b21168a41162207719b532e
Scott Hellings <scott.hellings@netmatters-scs.com>
2022-02-04 13:52:04 +0000

    fixed email layout

--- resources/views/emails/sendquote.blade.php
+++ resources/views/emails/sendquote.blade.php
@@ -1,28 +1,27 @@
 @component('mail::message')
+# Please see your quote details:
 
-    # Please see your quote details:
+**To:** {{ $quote->customer_name }}
 
-    **To:** {{ $quote->customer_name }}
+**Email:** {{$quote->customer_email}}
 
-    **Email:** {{$quote->customer_email}}
-
-    @component('mail::table')
-        |Product           | Qty              | Unit cost       | Total                          |
-        | :----------------: | :---------------:  | :---------------: | :------------------------------: |
-        @foreach($quote->products as $p)
-            |{{$p->name}}      |{{$p->pivot->qty}}|£{{number_format($p->price,2)}}   |£ {{number_format($p->pivot->qty *
+@component('mail::table')
+|Product           | Qty              | Unit cost       | Total                          |
+| :----------------: | :---------------:  | :---------------: | :------------------------------: |
+@foreach($quote->products as $p)
+|{{$p->name}}      |{{$p->pivot->qty}}|£{{number_format($p->price,2)}}   |£ {{number_format($p->pivot->qty *
 $p->price,2)}}|
-        @endforeach
-    @endcomponent
+@endforeach
+@endcomponent
 
-    ___
-    **Subtotal:** £{{number_format($quote->total/1.2, 2)}}
+___
+**Subtotal:** £{{number_format($quote->total/1.2, 2)}}
 
-    **VAT:** £{{number_format($quote->total - $quote->total/1.2, 2 )}}
+**VAT:** £{{number_format($quote->total - $quote->total/1.2, 2 )}}
 
-    **Total:** £{{number_format( $quote->total,2) }}
+**Total:** £{{number_format( $quote->total,2) }}
 
 
-    Thanks,<br>
-    {{ config('app.name') }}
+Thanks,<br>
+{{ config('app.name') }}
 @endcomponent
